52293ba83af26431cc7570a57995c57a81a91cbf
Prakhar Gupta <prakhargupta@localhost.localdomain>
2019-08-04 20:04:18 +0530

    Unit test for core logic

--- src/test/java/com/fast/discovery/coretests/DiscoveryHandlerTest.java
+++ src/test/java/com/fast/discovery/coretests/DiscoveryHandlerTest.java
@@ -3,25 +3,20 @@ package com.fast.discovery.coretests;
 import com.fast.discovery.core.DiscoveryHandler;
 import com.fast.discovery.dto.Acknowledgement;
 import com.fast.discovery.dto.ServiceRegistry;
-import org.junit.Assert;
+import com.fast.discovery.utility.Utilities;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.validation.constraints.AssertTrue;
-
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @Desc - Discovery Core logic tests.
  * @Author -
  */
 public class DiscoveryHandlerTest {
-    @Autowired
-    DiscoveryHandler discoveryHandler;
+
     @Test
     public void serviceRegistrationPositiveAckTest(){
+        DiscoveryHandler discoveryHandler = new DiscoveryHandler();
         ServiceRegistry sRegistry = new ServiceRegistry();
         sRegistry.setInstanceId("UniqueInstanceId");
         ServiceRegistry.InstanceInfo insInfo = new ServiceRegistry.InstanceInfo();
@@ -30,5 +25,53 @@ public class DiscoveryHandlerTest {
         Acknowledgement ack = discoveryHandler.serviceRegistration(sRegistry);
         assertTrue(ack.isAck());
     }
+    @Test
+    public void serviceRegistrationPositiveMsgTest(){
+        DiscoveryHandler discoveryHandler = new DiscoveryHandler();
+        ServiceRegistry sRegistry = new ServiceRegistry();
+        sRegistry.setInstanceId("UniqueInstanceId-1");
+        ServiceRegistry.InstanceInfo insInfo = new ServiceRegistry.InstanceInfo();
+        insInfo.setServiceName("Dummy Micro-Service");
+        sRegistry.setInstanceInformation(insInfo);
+        Acknowledgement ack = discoveryHandler.serviceRegistration(sRegistry);
+        assertEquals(ack.getMsg(), Utilities.SERVICE_REGISTERED);
+    }
 
+    @Test
+    public void serviceRegistrationNegativeAckTest(){
+        DiscoveryHandler discoveryHandler = new DiscoveryHandler();
+        ServiceRegistry s1Registry = new ServiceRegistry();
+        s1Registry.setInstanceId("UniqueInstanceId-2");
+        ServiceRegistry.InstanceInfo insInfo1 = new ServiceRegistry.InstanceInfo();
+        insInfo1.setServiceName("Dummy Micro-Service");
+        s1Registry.setInstanceInformation(insInfo1);
+        Acknowledgement ackS1 = discoveryHandler.serviceRegistration(s1Registry);
+        assertTrue(ackS1.isAck());
+        ServiceRegistry s2Registry = new ServiceRegistry();
+        s1Registry.setInstanceId("UniqueInstanceId-2");
+        ServiceRegistry.InstanceInfo insInfo2 = new ServiceRegistry.InstanceInfo();
+        insInfo2.setServiceName("Dumb Micro-Service");
+        s1Registry.setInstanceInformation(insInfo2);
+        Acknowledgement ackS2 = discoveryHandler.serviceRegistration(s1Registry);
+        assertFalse(ackS2.isAck());
+    }
+
+    @Test
+    public void serviceRegistrationNegativeMsgTest(){
+        DiscoveryHandler discoveryHandler = new DiscoveryHandler();
+        ServiceRegistry s1Registry = new ServiceRegistry();
+        s1Registry.setInstanceId("UniqueInstanceId-3");
+        ServiceRegistry.InstanceInfo insInfo1 = new ServiceRegistry.InstanceInfo();
+        insInfo1.setServiceName("Dummy Micro-Service");
+        s1Registry.setInstanceInformation(insInfo1);
+        Acknowledgement ackS1 = discoveryHandler.serviceRegistration(s1Registry);
+        assertEquals(ackS1.getMsg(), Utilities.SERVICE_REGISTERED);
+        ServiceRegistry s2Registry = new ServiceRegistry();
+        s1Registry.setInstanceId("UniqueInstanceId-3");
+        ServiceRegistry.InstanceInfo insInfo2 = new ServiceRegistry.InstanceInfo();
+        insInfo2.setServiceName("Dumb Micro-Service");
+        s1Registry.setInstanceInformation(insInfo2);
+        Acknowledgement ackS2 = discoveryHandler.serviceRegistration(s1Registry);
+        assertEquals(ackS2.getMsg(), Utilities.SERVICE_ALREADY_REGISTERED);
+    }
 }
