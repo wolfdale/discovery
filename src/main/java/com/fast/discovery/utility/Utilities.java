a5e515123c25ff3403fdb096e728238bf0aa778e
Prakhar Gupta <prakhargupta@localhost.localdomain>
2019-08-04 18:53:58 +0530

    WorkTree API

---
+++ src/main/java/com/fast/discovery/utility/Utilities.java
@@ -0,0 +1,11 @@
+package com.fast.discovery.utility;
+
+public class Utilities {
+
+    /* Service registration ACK */
+    public static String SERVICE_REGISTERED = "Service registered with Discovery !";
+
+    /* Service registration NACK */
+    public static String SERVICE_ALREADY_REGISTERED = "This service already registered with Discovery.";
+
+}
